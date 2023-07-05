c7e29d622896b40ec3d9c3acd729ca9d8eada620
aswinj18 <aswinjshaji@gmail.com>
2023-07-05 20:39:43 +0530

    Solution to todoServer

--- week-2/Week-2-Assignments/02-nodejs/todoServer.js
+++ week-2/Week-2-Assignments/02-nodejs/todoServer.js
@@ -46,4 +46,80 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// Datastructure storing data
+var todoData = {};
+var nxtIdx = 1;
+
+// 1.GET /todos - Retrieve all todo items
+// Description: Returns a list of all todo items.
+//     Response: 200 OK with an array of todo items in JSON format.
+//     Example: GET http://localhost:3000/todos
+app.get('/todos', (req, res) => {
+  var responseBody = Object.entries(todoData).map(([key, val]) => {
+    return {
+      id: key,
+      ...val
+    };
+  });
+  res.status(200).send(responseBody);
+})
+
+// 2.GET /todos/:id - Retrieve a specific todo item by ID
+// Description: Returns a specific todo item identified by its ID.
+// Response: 200 OK with the todo item in JSON format if found, or 404 Not Found if not found.
+// Example: GET http://localhost:3000/todos/123
+app.get('/todos/:id', (req, res) => {
+  if (id in todoData) {
+    const specificToDo = todoData[id];
+    const responseBody = {
+      id: id,
+      ...specificToDo
+    }
+    return res.status(200).send(responseBody);
+  } else {
+    return res.status(404).send();
+  }
+});
+
+// 3. POST /todos - Create a new todo item
+// Description: Creates a new todo item.
+// Request Body: JSON object representing the todo item.
+// Response: 201 Created with the ID of the created todo item in JSON format. eg: {id: 1}
+// Example: POST http://localhost:3000/todos
+// Request Body: { "title": "Buy groceries", "completed": false, description: "I should buy groceries" }
+app.post('/todos', (req, res) => {
+  var incomingData = req.body;
+
+  id = nxtIdx;
+  nxtIdx += 1;
+
+  todoData[id] = incomingData;
+
+  var responseBody = {
+    id: id
+  }
+  res.status(201).send(responseBody)
+})
+
+// 4. PUT /todos/:id - Update an existing todo item by ID
+// Description: Updates an existing todo item identified by its ID.
+// Request Body: JSON object representing the updated todo item.
+// Response: 200 OK if the todo item was found and updated, or 404 Not Found if not found.
+// Example: PUT http://localhost:3000/todos/123
+// Request Body: { "title": "Buy groceries", "completed": true }
+app.put('/todos/:id', (req, res) => {
+  const incomingData = req.body;
+  todoData[id] = incomingData;
+  res.status(200).send();
+})
+
+// 5. DELETE /todos/:id - Delete a todo item by ID
+// Description: Deletes a todo item identified by its ID.
+// Response: 200 OK if the todo item was found and deleted, or 404 Not Found if not found.
+// Example: DELETE http://localhost:3000/todos/123
+app.delete('/todos/:id', (req, res) => {
+  delete todoData[id];
+  res.status(200).send();
+})
+
 module.exports = app;
