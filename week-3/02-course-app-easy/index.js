916816c844d7dfe54cb36e90e6acc3e0542d49f6
aswinj18 <aswinjshaji@gmail.com>
2023-07-08 19:03:30 +0530

    Solved 02-course-app-easy

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -6,47 +6,195 @@ app.use(express.json());
 let ADMINS = [];
 let USERS = [];
 let COURSES = [];
+let USERPURCHASEDCOURSES = {};
+
+function checkAdminCredValidity(adminCred) {
+  const matchingAdminId = ADMINS.findIndex(item => (item.username===adminCred.username) && (item.password===adminCred.password));
+  if (matchingAdminId !== -1) {
+    res = true;
+  } else {
+    res = false;
+  }
+  return res;
+}
+
+function checkUserCredValidity(userCred) {
+  const matchingUserId = USERS.findIndex(item => (item.username===userCred.username) && (item.password===userCred.password));
+  if (matchingUserId !== -1) {
+    res = true;
+  } else {
+    res = false;
+  }
+  return res;
+}
+
+function getUserId(userCred) {
+  const matchingUserId = USERS.findIndex(item => (item.username===userCred.username) && (item.password===userCred.password));
+  return matchingUserId;
+}
 
 // Admin routes
 app.post('/admin/signup', (req, res) => {
-  // logic to sign up admin
+  const incomingAdminDetails = req.body;
+  const matchingAdminId = ADMINS.findIndex(item => item.username === incomingAdminDetails.username);
+  if (matchingAdminId === -1) {
+    ADMINS.push(incomingAdminDetails);
+    res.json({message: 'Admin created successfully'});
+  } else {
+    ADMINS[ matchingAdminId ] = incomingAdminDetails;
+    res.json({message: 'Admin updated successfully'});
+  }
 });
 
 app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+  const incomingAdminCredentials = req.headers;
+  const isAdminCredValid = checkAdminCredValidity(incomingAdminCredentials);
+  if (isAdminCredValid) {
+    res.json({ message: 'Logged in successfully' });
+  } else {
+    res.status(403).json({ message: 'Wrong Credentials!!' });
+  }
 });
 
 app.post('/admin/courses', (req, res) => {
-  // logic to create a course
+  const incomingAdminCredentials = req.headers;
+  const isAdminCredValid = checkAdminCredValidity(incomingAdminCredentials);
+
+  if (isAdminCredValid) {
+
+    const incomingCourse = req.body;
+    COURSES.push(incomingCourse);
+    const courseId = COURSES.length;
+
+    const responseBody = {
+      message: 'Course created successfully', 
+      courseId: courseId
+    }
+    res.json(responseBody);
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  } 
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
-  // logic to edit a course
+  const incomingAdminCredentials = req.headers;
+  const isAdminCredValid = checkAdminCredValidity(incomingAdminCredentials);
+
+  if (isAdminCredValid) {
+
+    const courseId = req.params.courseId;
+    const courseIndex = courseId - 1;
+
+    const incomingUpdatedCourse = req.body;
+    COURSES[ courseIndex ] = incomingUpdatedCourse;
+
+    res.json({ message: 'Course updated successfully' });
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  } 
 });
 
 app.get('/admin/courses', (req, res) => {
-  // logic to get all courses
+  const incomingAdminCredentials = req.headers;
+  const isAdminCredValid = checkAdminCredValidity(incomingAdminCredentials);
+
+  if (isAdminCredValid) {
+
+    const responseBody = COURSES.map((item,index) => ({
+      id: index + 1,
+      ...item
+    }))
+    res.json(responseBody);
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  } 
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
-  // logic to sign up user
+  const incomingUserDetails = req.body;
+  const matchingUserId = USERS.findIndex(item => item.username === incomingUserDetails.username);
+  if (matchingUserId === -1) {
+    USERS.push(incomingUserDetails);
+    res.json({message: 'User created successfully'});
+  } else {
+    USERS[ matchingUserId ] = incomingUserDetails;
+    res.json({message: 'User updated successfully'});
+  }
 });
 
 app.post('/users/login', (req, res) => {
-  // logic to log in user
+  const incomingUserCredentials = req.headers;
+  const isUserCredValid = checkUserCredValidity(incomingUserCredentials);
+  if (isUserCredValid) {
+    res.json({ message: 'Logged in successfully' });
+  } else {
+    res.status(403).json({ message: 'Wrong Credentials!!' });
+  }
 });
 
 app.get('/users/courses', (req, res) => {
-  // logic to list all courses
+  const incomingUserCredentials = req.headers;
+  const isUserCredValid = checkUserCredValidity(incomingUserCredentials);
+
+  if (isUserCredValid) {
+
+    const responseBody = COURSES.map((item,index) => ({
+      id: index + 1,
+      ...item
+    }))
+    res.json(responseBody);
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  } 
 });
 
 app.post('/users/courses/:courseId', (req, res) => {
-  // logic to purchase a course
+  const incomingUserCredentials = req.headers;
+  const userIndex = getUserId(incomingUserCredentials);
+
+  if (userIndex !== -1) {
+
+    const courseId = parseInt(req.params.courseId);
+    const courseIndex = courseId - 1;
+
+    if (userIndex in USERPURCHASEDCOURSES) {
+      USERPURCHASEDCOURSES[ userIndex ].push(courseIndex);
+    } else {
+      USERPURCHASEDCOURSES[ userIndex ] = [ courseIndex ];
+    }
+
+    res.json({ message: 'Course purchased successfully' });
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  }
 });
 
 app.get('/users/purchasedCourses', (req, res) => {
-  // logic to view purchased courses
+  const incomingUserCredentials = req.headers;
+  const userIndex = getUserId(incomingUserCredentials);
+
+  if (userIndex !== -1) {
+
+    const purchasedCoursesId = USERPURCHASEDCOURSES[ userIndex ];
+    const purchasedCoursesDetails = purchasedCoursesId.map((item) => ({
+      id: item + 1,
+      ...COURSES[item]
+    }));
+
+    const responseBody = {
+      purchasedCourses: purchasedCoursesDetails
+    };
+    res.json(responseBody);
+
+  } else {
+    res.status(401).json({'message': 'Wrong Credentials'});
+  }
 });
 
 app.listen(3000, () => {
